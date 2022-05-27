833ff9f202ca8220e6cedf77988be7ca27955f97
George Fischer <me@george.id.au>
2022-05-27 19:53:42 +1000

    make pagination nicer to use

--- app/lib/components/pagination.tsx
+++ app/lib/components/pagination.tsx
@@ -1,5 +1,7 @@
 import { Form } from "@remix-run/react";
 
+const buttonClass =
+  "p-2 rounded shadow text-inherit disabled:opacity-50 disabled:shadow-none";
 const Pagination: React.FC<{
   children?: never;
   currentPage: number;
@@ -7,112 +9,101 @@ const Pagination: React.FC<{
 }> = ({ currentPage, pages }) => (
   <Form className="flex justify-between items-center text-black dark:text-neutral-300">
     <div className="flex gap-2">
-      {currentPage > 2 ? (
-        <button
-          type="submit"
-          name="page"
-          value={1}
-          aria-label="First Page"
-          className="p-2 rounded shadow text-inherit"
+      <button
+        type="submit"
+        name="page"
+        value={1}
+        aria-label="First Page"
+        className={buttonClass}
+        disabled={currentPage <= 1}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-      ) : (
-        ""
-      )}
-      {currentPage > 1 ? (
-        <button
-          type="submit"
-          name="page"
-          value={currentPage - 1}
-          aria-label="Previous Page"
-          className="p-2 rounded shadow text-inherit"
+          <path
+            fillRule="evenodd"
+            d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+
+      <button
+        type="submit"
+        name="page"
+        value={currentPage - 1}
+        aria-label="Previous Page"
+        className={buttonClass}
+        disabled={currentPage <= 1}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-      ) : (
-        ""
-      )}
+          <path
+            fillRule="evenodd"
+            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
     </div>
     <div className="">
       Page {currentPage} of {pages}
     </div>
     <div className="flex gap-2">
-      {currentPage < pages ? (
-        <button
-          type="submit"
-          name="page"
-          value={currentPage + 1}
-          aria-label="Next Page"
-          className="p-2 rounded shadow text-inherit"
+      <button
+        type="submit"
+        name="page"
+        value={currentPage + 1}
+        aria-label="Next Page"
+        className={buttonClass}
+        disabled={currentPage >= pages}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-      ) : (
-        ""
-      )}
-      {currentPage < pages - 1 ? (
-        <button
-          type="submit"
-          name="page"
-          value={pages}
-          aria-label="Last Page"
-          className="p-2 rounded shadow text-inherit"
+          <path
+            fillRule="evenodd"
+            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <button
+        type="submit"
+        name="page"
+        value={pages}
+        aria-label="Last Page"
+        className={buttonClass}
+        disabled={currentPage >= pages}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
-              clipRule="evenodd"
-            />
-            <path
-              fillRule="evenodd"
-              d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-      ) : (
-        ""
-      )}
+          <path
+            fillRule="evenodd"
+            d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+            clipRule="evenodd"
+          />
+          <path
+            fillRule="evenodd"
+            d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
     </div>
   </Form>
 );
