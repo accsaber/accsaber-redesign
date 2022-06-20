825478cbaf8333d838defa870b2c1e9b7c7512d9
George Fischer <me@george.id.au>
2022-06-20 11:29:57 +1000

    Hide campaign menu item until it's actually usable

--- app/lib/headerItems.ts
+++ app/lib/headerItems.ts
@@ -3,7 +3,7 @@ import type HeaderItem from "./interfaces/components/header/item";
 const headerItems: HeaderItem[] = [
   { name: "Leaderboards", href: "/leaderboards", icon: "list" },
   { name: "Ranked Maps", href: "/maps", icon: "map" },
-  { name: "Campaign", href: "/campaign", icon: "emoji_events" },
+  // { name: "Campaign", href: "/campaign", icon: "emoji_events" },
 ];
 
 export default headerItems;
