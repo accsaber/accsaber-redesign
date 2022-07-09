92cdc2173def2305b590f5aa0cce342f822af3b6
George Fischer <me@george.id.au>
2022-07-10 02:14:34 +1000

    switch to replicated redis

--- app/lib/api/config/config.prod.ts
+++ app/lib/api/config/config.prod.ts
@@ -4,10 +4,7 @@ const productionConfig: ApiConfig = {
   apiURL: "https://api.accsaber.com/",
   cdnURL: "https://accsaber.com/cdn/",
   imageURL: "https://images.accsaber.com/",
-  redisURL:
-    typeof "process" !== "undefined"
-      ? process?.env["FLY_REDIS_CACHE_URL"] ?? ""
-      : "",
+  redisURL: "redis://accsaber-redis.internal:6379",
   defaultLocale: "en-AU",
   isBeta: false,
 };
