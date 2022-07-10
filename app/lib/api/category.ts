bd85717ff98f0a92a6db1bd2c5f4920cb4afe9a9
George Fischer <me@george.id.au>
2022-07-10 17:33:12 +1000

    Fix category sort order
    Fixes #25

--- app/lib/api/category.ts
+++ app/lib/api/category.ts
@@ -1,22 +1,35 @@
 import type { Category } from "../interfaces/api/category";
 import apiFetcher, { client } from "./fetcher";
 
+const order = ["true", "standard", "tech"];
+
 export const getCategories = async () => {
   const key = "accsaber:categories";
   if (!client.isOpen) await client.connect();
 
-  const cachedCategories = Object.entries(await client.hGetAll(key));
+  const cachedCategories = Object.values(await client.hGetAll(key));
 
-  let categories = new Map(
-    cachedCategories.map(([name, category]) => {
-      const parsed = JSON.parse(category) as Category;
-      return [parsed.categoryName, parsed];
-    })
-  );
+  let sorted: [string, Category][] = cachedCategories.map((category) => {
+    const parsed = JSON.parse(category) as Category;
+    return [parsed.categoryName, parsed];
+  });
+
+  sorted.sort(([_a, a], [_b, b]) => {
+    const itemValue = (item: Category) =>
+      order.indexOf(item.categoryName) ?? order.length;
+    return itemValue(a as Category) - itemValue(b as Category);
+  });
+
+  let categories = new Map(sorted);
 
   if (categories.size == 0) {
     console.log(`refreshing category list`);
     const { data } = await apiFetcher.get<Category[]>("/categories");
+    data.sort((a, b) => {
+      const itemValue = (item: Category) =>
+        order.indexOf(item.categoryName) ?? order.length;
+      return itemValue(a as Category) - itemValue(b as Category);
+    });
     for (const category of data) {
       const transaction = client.multi();
       transaction.hSet(key, category.categoryName, JSON.stringify(category));
