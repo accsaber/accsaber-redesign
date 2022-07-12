b66f42061392a1a444f96256253a7af5b156e623
George Fischer <me@george.id.au>
2022-07-12 16:06:52 +1000

    split search button into separate component

--- app/lib/components/header.tsx
+++ app/lib/components/header.tsx
@@ -2,6 +2,7 @@ import { Link, NavLink } from "@remix-run/react";
 import logo from "~/lib/images/logo.webp";
 import headerItems from "../headerItems";
 import DarkToggle from "./darkToggle";
+import SearchButton from "./searchButton";
 import UserContext from "./userContext";
 
 const Header = () => {
@@ -24,20 +25,7 @@ const Header = () => {
             Wiki
           </a>
         </nav>
-        <NavLink to="/search" className={"headerNav p-3"} aria-label="Search">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-5 h-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </NavLink>
+        <SearchButton />
         <DarkToggle />
         <UserContext.Consumer>
           {(user) =>
