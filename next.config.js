035ddb6f630f5281df8c1956bbade863b11eb3e8
George Fischer <me@george.id.au>
2022-10-27 13:09:32 +0200

    put the edge runtime config in the right block

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: { appDir: true },
-  runtime: "experimental-edge",
+  experimental: { appDir: true, runtime: "experimental-edge" },
   images: {
     domains: ["cdn.scoresaber.com", "cdn.accsaber.com"],
   },
