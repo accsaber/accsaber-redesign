70a29226f42bfb91787c7a5df1a5c03b296b9896
George Fischer <me@george.id.au>
2022-11-18 13:01:27 +0100

    try and make it work on cf

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
     enableUndici: true,
+    runtime: "experimental-edge",
   },
   images: {
     domains: ["cdn.scoresaber.com", "cdn.accsaber.com"],
