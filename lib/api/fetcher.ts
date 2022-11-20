dfe0b28d60fba23eab91b8988d4454e5a7400b4c
George Fischer <me@george.id.au>
2022-11-20 18:48:55 +0100

    add caching to the json fetcher

--- lib/api/fetcher.ts
+++ lib/api/fetcher.ts
@@ -1,4 +1,5 @@
 import type { Player } from "$interfaces/api/player";
+import { cache } from "react";
 import config from "./config";
 
 export interface ErrorResponse {
@@ -7,19 +8,22 @@ export interface ErrorResponse {
 }
 
 const apiFetch = async (target: string | URL, init?: RequestInit) => {
-  const apiRequest = new Request(new URL(target, config.apiURL), init);
-  return fetch(apiRequest);
+  return fetch(new URL(target, config.apiURL), {
+    next: { revalidate: 60 * 5 },
+    ...init,
+  });
 };
 
 export const isErrorResponse = (response: any): response is ErrorResponse =>
   typeof response.message == "string" && typeof response.errorCode == "string";
 
-export const json = <T>(...params: Parameters<typeof apiFetch>) =>
-  apiFetch(...params).then(async (response) => {
+export const json = cache(<T>(target: string) =>
+  apiFetch(target).then(async (response) => {
     const data = (await response.json()) as T | ErrorResponse;
     if (isErrorResponse(data) || response.status >= 400) throw data;
     else return data;
-  });
+  })
+);
 
 export const getPlayer = (userId: string, category = "overall") =>
   json<Player>(`players/${encodeURIComponent(userId)}/${category}`);
