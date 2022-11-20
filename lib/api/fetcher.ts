4f446a5703fc11eb40ddd6f4c7a4354f769791a8
George Fischer <me@george.id.au>
2022-11-20 20:36:07 +0100

    Add long load logs

--- lib/api/fetcher.ts
+++ lib/api/fetcher.ts
@@ -1,4 +1,5 @@
 import type { Player } from "$interfaces/api/player";
+import ms from "ms";
 import { cache } from "react";
 import config from "./config";
 
@@ -8,9 +9,22 @@ export interface ErrorResponse {
 }
 
 const apiFetch = async (target: string | URL, init?: RequestInit) => {
+  const measurementTarget =
+    typeof performance !== "undefined" ? performance : Date;
+  const startTime = measurementTarget.now();
   return fetch(new URL(target, config.apiURL), {
     next: { revalidate: 60 * 5 },
     ...init,
+  }).then((response) => {
+    const finishTime = measurementTarget.now();
+    const duration = finishTime - startTime;
+    if (duration >= 100)
+      console.warn(
+        `Loading \x1b[97;1m${target}\x1b[0m took \x1b[1;3${
+          duration >= 200 ? "1" : "3"
+        }m${ms(duration)}\x1b[0m`
+      );
+    return response;
   });
 };
 
