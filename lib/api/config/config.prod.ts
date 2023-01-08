ca9923cdc28389e6c028c38836c0bbfe0e2c5f61
George Fischer <me@george.id.au>
2023-01-08 16:51:16 +0100

    use accsaber dev url

--- lib/api/config/config.prod.ts
+++ lib/api/config/config.prod.ts
@@ -1,9 +1,9 @@
 import type ApiConfig from "../../interfaces/config";
 
 const productionConfig: ApiConfig = {
-	apiURL: "https://api.accsaber.com/",
-	cdnURL: "https://cdn.accsaber.com/",
-	gqlURL: "https://gql.accsaber.com/graphql",
+	apiURL: "https://api.accsaber-dev.ixsen.de",
+	cdnURL: "https://cdn.accsaber-dev.ixsen.de",
+	gqlURL: "https://graphile.accsaber-dev.ixsen.de/graphql",
 	defaultLocale: "en-AU",
 	isBeta: true,
 };
