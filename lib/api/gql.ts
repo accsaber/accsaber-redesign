18feea5d223ae9ea8f53d44750d669a3e740c15e
George Fischer <me@george.id.au>
2023-01-11 18:23:53 +0100

    gql cachery

--- lib/api/gql.ts
+++ lib/api/gql.ts
@@ -1,6 +1,12 @@
 import { GraphQLClient } from "graphql-request";
+import { cache } from "react";
 import { getSdk } from "../__generated__/gql";
 import config from "./config";
 
 export const client = new GraphQLClient(config.gqlURL);
 export const sdk = getSdk(client);
+
+for (const i of Object.keys(sdk) as (keyof typeof sdk)[]) {
+  // @ts-expect-error hackery
+  sdk[i] = cache(sdk[i]);
+}
