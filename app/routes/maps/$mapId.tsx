cdc972e82fc730d3854b35fc395f9e5ebf34c909
George Fischer <me@george.id.au>
2023-02-14 11:28:58 +0100

    Add header to map pages

--- app/routes/maps/$mapId.tsx
+++ app/routes/maps/$mapId.tsx
@@ -5,6 +5,7 @@ import CDNImage from "@/CDNImage";
 import Complexity from "@/Complexity";
 import DifficultyLabel from "@/DifficultyLabel";
 import LoadingSpinner from "@/LoadingSpinner";
+import PageHeader from "@/PageHeader";
 import Pagination from "@/Pagination";
 import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import { json as jsonResponse } from "@remix-run/node";
@@ -15,6 +16,7 @@ import invariant from "tiny-invariant";
 import { language } from "~/lib/api/config";
 import { json } from "~/lib/api/fetcher";
 import { gqlClient } from "~/lib/api/gql";
+import scoresaberLogo from "~/images/scoresaber.svg";
 
 const pageSize = 25;
 
@@ -73,128 +75,177 @@ export default function MapPage() {
   } = useLoaderData<MapPageData>();
 
   return (
-    <main>
-      <div className="relative overflow-hidden bg-neutral-100 dark:bg-black/20">
-        <CDNImage
-          src={`covers/${map?.song?.songHash.toUpperCase()}.png`}
-          alt=""
-          className="absolute top-0 left-0 object-cover w-full h-full opacity-20 blur-3xl"
-          width={256}
-          height={256}
-        />
-        <div
-          className={[
-            "flex flex-col md:flex-row gap-6 py-16 text-neutral-800 dark:text-neutral-200 items-center",
-            "max-w-screen-lg mx-auto px-4",
-          ].join(" ")}
-        >
-          <div className="flex w-32 h-32 overflow-hidden rounded-lg shadow-lg aspect-square">
-            <CDNImage
-              src={`covers/${map?.song?.songHash.toUpperCase()}.png`}
-              alt=""
-              width={256}
-              height={256}
-            />
+    <>
+      <PageHeader
+        image={`covers/${map?.song?.songHash.toUpperCase()}.png`}
+        transparent
+        hideTitleUntilScrolled
+        actionButton={
+          <div className="flex">
+            <a
+              href={`https://beatsaver.com/maps/${map?.song?.beatSaverKey}`}
+              className="block p-2 rounded-full hover:bg-neutral-100 dark:hover:bg-neutral-700"
+              aria-label="Download"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                />
+              </svg>
+            </a>
+            <a
+              href={`https://scoresaber.com/leaderboard/${map?.leaderboardId}`}
+              className="block p-2 rounded-full hover:bg-neutral-100 dark:hover:bg-neutral-700"
+              aria-label="ScoreSaber"
+            >
+              <img
+                src={scoresaberLogo}
+                alt="Map Leaderboard on ScoreSaber"
+                className="h-6"
+              />
+            </a>
           </div>
-          <div className="flex flex-col gap-1">
-            <h1 className="flex flex-col gap-2 text-2xl font-bold md:flex-row">
-              {map?.song?.songAuthorName} - {map?.song?.songName}
-              {map?.song?.songSubName ? (
-                <small>{map?.song?.songSubName}</small>
-              ) : (
-                ""
-              )}
-              <DifficultyLabel>
-                {(map?.difficulty ?? "").toString()}
-              </DifficultyLabel>
-            </h1>
-            <h2 className="text-xl">{map?.category?.categoryDisplayName}</h2>
-            <h2 className="text-xl">
-              Mapped by <strong>{map?.song?.levelAuthorName}</strong>
-            </h2>
-            <Complexity>{map?.complexity ?? 0}</Complexity>
+        }
+      >
+        {map?.song?.songAuthorName} - {map?.song?.songName}
+      </PageHeader>
+
+      <main>
+        <div className="relative overflow-hidden bg-neutral-100 dark:bg-black/20">
+          <CDNImage
+            src={`covers/${map?.song?.songHash.toUpperCase()}.png`}
+            alt=""
+            className="absolute top-0 left-0 object-cover w-full h-full opacity-20 blur-3xl"
+            width={256}
+            height={256}
+          />
+          <div
+            className={[
+              "flex flex-col md:flex-row gap-6 py-16 text-neutral-800 dark:text-neutral-200 items-center",
+              "max-w-screen-lg mx-auto px-4",
+            ].join(" ")}
+          >
+            <div className="flex w-32 h-32 overflow-hidden rounded-lg shadow-lg aspect-square">
+              <CDNImage
+                src={`covers/${map?.song?.songHash.toUpperCase()}.png`}
+                alt=""
+                width={256}
+                height={256}
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <h1 className="flex flex-col gap-2 text-2xl font-bold md:flex-row">
+                {map?.song?.songAuthorName} - {map?.song?.songName}
+                {map?.song?.songSubName ? (
+                  <small>{map?.song?.songSubName}</small>
+                ) : (
+                  ""
+                )}
+                <DifficultyLabel>
+                  {(map?.difficulty ?? "").toString()}
+                </DifficultyLabel>
+              </h1>
+              <h2 className="text-xl">{map?.category?.categoryDisplayName}</h2>
+              <h2 className="text-xl">
+                Mapped by <strong>{map?.song?.levelAuthorName}</strong>
+              </h2>
+              <Complexity>{map?.complexity ?? 0}</Complexity>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="max-w-screen-lg p-4 mx-auto">
-        <Pagination pages={pages} currentPage={page} />
-      </div>
-      <div className="max-w-full mx-auto overflow-auto prose md:max-w-screen-lg dark:prose-invert whitespace-nowrap">
-        <table>
-          <thead>
-            <tr>
-              <th></th>
-              <th></th>
-              <th>Player Name</th>
-              <th>Time Set</th>
-              <th>Accuracy</th>
-              <th>AP</th>
-              <th>Score</th>
-            </tr>
-          </thead>
-          <tbody>
-            {leaderboard.map((score, i) => (
-              <tr key={score.playerId}>
-                <td>#{score.rank}</td>
-                <td className="relative w-10 min-w-[2.5rem] aspect-square">
-                  <NavLink to={`/profile/${score.playerId}/overall/scores`}>
-                    {({ isPending }) => (
-                      <>
-                        <div className="absolute top-0 left-0 w-10 h-10">
-                          <Avatar square variant="beam" name={score.playerId} />
-                        </div>
-
-                        {score.playerId.startsWith("7") && (
-                          <CDNImage
-                            src={`avatars/${score.playerId}.jpg`}
-                            alt={`${score.playerName}'s profile`}
-                            loading="lazy"
-                            className="absolute top-0 left-0 m-0"
-                            width={40}
-                            height={40}
-                          />
-                        )}
-                        {isPending && (
-                          <div className="absolute top-0 left-0 w-10 h-10 flex bg-white/80 dark:bg-black/80">
-                            <LoadingSpinner className="p-2 w-10 h-10" />
-                          </div>
-                        )}
-                      </>
-                    )}
-                  </NavLink>
-                </td>
-                <td>
-                  <Link to={`/profile/${score.playerId}/overall/scores`}>
-                    {score.playerName}
-                  </Link>
-                </td>
-                <td
-                  title={new Date(score.timeSet).toLocaleString(language)}
-                  className="whitespace-nowrap"
-                >
-                  {DateTime.fromISO(score.timeSet).toRelative()}
-                </td>
-                <td>
-                  {(score.accuracy * 100).toLocaleString(language, {
-                    maximumFractionDigits: 2,
-                  })}
-                  %
-                </td>
-                <td>
-                  {score.ap.toLocaleString(language, {
-                    maximumFractionDigits: 2,
-                  })}
-                </td>
-                <td>{score.score}</td>
+        <div className="max-w-screen-lg p-4 mx-auto">
+          <Pagination pages={pages} currentPage={page} />
+        </div>
+        <div className="max-w-full mx-auto overflow-auto prose md:max-w-screen-lg dark:prose-invert whitespace-nowrap">
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th></th>
+                <th>Player Name</th>
+                <th>Time Set</th>
+                <th>Accuracy</th>
+                <th>AP</th>
+                <th>Score</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {leaderboard.map((score, i) => (
+                <tr key={score.playerId}>
+                  <td>#{score.rank}</td>
+                  <td className="relative w-10 min-w-[2.5rem] aspect-square">
+                    <NavLink to={`/profile/${score.playerId}/overall/scores`}>
+                      {({ isPending }) => (
+                        <>
+                          <div className="absolute top-0 left-0 w-10 h-10">
+                            <Avatar
+                              square
+                              variant="beam"
+                              name={score.playerId}
+                            />
+                          </div>
 
-      <div className="max-w-screen-lg p-4 mx-auto">
-        <Pagination pages={pages} currentPage={page} />
-      </div>
-    </main>
+                          {score.playerId.startsWith("7") && (
+                            <CDNImage
+                              src={`avatars/${score.playerId}.jpg`}
+                              alt={`${score.playerName}'s profile`}
+                              loading="lazy"
+                              className="absolute top-0 left-0 m-0"
+                              width={40}
+                              height={40}
+                            />
+                          )}
+                          {isPending && (
+                            <div className="absolute top-0 left-0 w-10 h-10 flex bg-white/80 dark:bg-black/80">
+                              <LoadingSpinner className="p-2 w-10 h-10" />
+                            </div>
+                          )}
+                        </>
+                      )}
+                    </NavLink>
+                  </td>
+                  <td>
+                    <Link to={`/profile/${score.playerId}/overall/scores`}>
+                      {score.playerName}
+                    </Link>
+                  </td>
+                  <td
+                    title={new Date(score.timeSet).toLocaleString(language)}
+                    className="whitespace-nowrap"
+                  >
+                    {DateTime.fromISO(score.timeSet).toRelative()}
+                  </td>
+                  <td>
+                    {(score.accuracy * 100).toLocaleString(language, {
+                      maximumFractionDigits: 2,
+                    })}
+                    %
+                  </td>
+                  <td>
+                    {score.ap.toLocaleString(language, {
+                      maximumFractionDigits: 2,
+                    })}
+                  </td>
+                  <td>{score.score}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <div className="max-w-screen-lg p-4 mx-auto">
+          <Pagination pages={pages} currentPage={page} />
+        </div>
+      </main>
+    </>
   );
 }
--- app/routes/maps/index.tsx
+++ app/routes/maps/index.tsx
@@ -2,8 +2,11 @@ import type { MapRowFragment, RankedMapsQuery } from "$gql";
 import { BeatMapsOrderBy, RankedMapsDocument } from "$gql";
 import GQLSortButton from "@/GQLSortButton";
 import MapRow from "@/MapRow";
+import PageHeader from "@/PageHeader";
 import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import { useLoaderData, useSearchParams } from "@remix-run/react";
+import { useRef } from "react";
+import config from "~/lib/api/config";
 import { gqlClient } from "~/lib/api/gql";
 
 export const loader: LoaderFunction = async ({ request }) => {
@@ -21,8 +24,9 @@ export const meta: MetaFunction = () => ({
 });
 
 export default function RankedMapsPage() {
-  const { beatMaps: maps } = useLoaderData<RankedMapsQuery>();
+  const { beatMaps: maps, categories } = useLoaderData<RankedMapsQuery>();
   const [searchParams] = useSearchParams();
+  const downloadMenu = useRef<HTMLDialogElement>();
   const columns: [(keyof typeof BeatMapsOrderBy)[] | null, string, number?][] =
     [
       [null, "Song Name", 2],
@@ -34,32 +38,86 @@ export default function RankedMapsPage() {
     ];
 
   return (
-    <div className="w-full max-w-screen-lg px-4 py-8 mx-auto prose dark:prose-invert">
-      <table>
-        <thead>
-          <tr>
-            {columns.map(([value, friendly, colSpan], n) => (
-              <th key={n} colSpan={colSpan}>
-                {value ? (
-                  <GQLSortButton
-                    values={value}
-                    currentValue={searchParams.get("sortBy") ?? undefined}
-                  >
-                    {friendly}
-                  </GQLSortButton>
-                ) : (
-                  friendly
-                )}
-              </th>
+    <>
+      <PageHeader
+        actionButton={
+          <button
+            onClick={() => downloadMenu.current?.showModal()}
+            className="px-4 py-2 shadow-md bg-white dark:bg-neutral-700 rounded text-inherit"
+          >
+            Download Playlists
+          </button>
+        }
+      >
+        Ranked Maps
+      </PageHeader>
+
+      <dialog
+        ref={(self) => self && (downloadMenu.current = self)}
+        className=" bg-white dark:bg-neutral-900 dark:text-neutral-200 p-0 w-full max-w-md rounded-lg shadow-xl"
+      >
+        <div className="flex flex-col">
+          <h2 className="text-2xl bg-neutral-100 dark:bg-neutral-800 px-6 py-4 flex justify-between">
+            Download Playlists
+            <button onClick={() => downloadMenu.current?.close()}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </h2>
+          <div className="flex flex-col p-4 gap-3">
+            {categories?.nodes.map(({ categoryName, categoryDisplayName }) => (
+              <div key={categoryName}>
+                <a
+                  href={new URL(
+                    `playlists/${categoryName}`,
+                    config.apiURL
+                  ).toString()}
+                  className="py-2 px-3 block w-max shadow-lg bg-blue-600 text-white rounded shadow-blue-600/50"
+                >
+                  Download {categoryDisplayName} playlist
+                </a>
+              </div>
             ))}
-          </tr>
-        </thead>
-        <tbody>
-          {maps?.nodes?.map((map) => (
-            <MapRow map={map as MapRowFragment} key={map.leaderboardId} />
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </div>
+        </div>
+      </dialog>
+      <div className="w-full max-w-screen-lg px-4 py-8 mx-auto prose dark:prose-invert">
+        <table>
+          <thead>
+            <tr>
+              {columns.map(([value, friendly, colSpan], n) => (
+                <th key={n} colSpan={colSpan}>
+                  {value ? (
+                    <GQLSortButton
+                      values={value}
+                      currentValue={searchParams.get("sortBy") ?? undefined}
+                    >
+                      {friendly}
+                    </GQLSortButton>
+                  ) : (
+                    friendly
+                  )}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {maps?.nodes?.map((map) => (
+              <MapRow map={map as MapRowFragment} key={map.leaderboardId} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
