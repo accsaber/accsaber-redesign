bdf8f683aa4cfe011200601f290e0caaea3a586f
George Fischer <me@george.id.au>
2023-02-27 10:21:34 +0100

    use FLY_REGION env variable as basis for gql route

--- app/lib/api/config/config.prod.ts
+++ app/lib/api/config/config.prod.ts
@@ -4,7 +4,7 @@ const productionConfig: ApiConfig = {
   apiURL: "https://api.accsaber-dev.ixsen.de",
   cdnURL: "https://cdn.accsaber-dev.ixsen.de",
   gqlURL:
-    typeof window !== "undefined"
+    typeof process !== "undefined" && process.env.FLY_REGION
       ? "https://gql.accsaber.com/graphql"
       : "http://accsaber-gql.internal:4000/graphql",
   defaultLocale: "en-AU",
