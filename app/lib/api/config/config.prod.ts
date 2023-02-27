b72a92c0a368235d903c4ce6dc77908791e75114
George Fischer <me@george.id.au>
2023-02-27 10:32:41 +0100

    i am still stupid

--- app/lib/api/config/config.prod.ts
+++ app/lib/api/config/config.prod.ts
@@ -3,10 +3,9 @@ import type ApiConfig from "../../interfaces/config";
 const productionConfig: ApiConfig = {
   apiURL: "https://api.accsaber-dev.ixsen.de",
   cdnURL: "https://cdn.accsaber-dev.ixsen.de",
-  gqlURL:
-    typeof process == "undefined" || process.env.FLY_REGION
-      ? "https://gql.accsaber.com/graphql"
-      : "http://accsaber-gql.internal:4000/graphql",
+  gqlURL: !(typeof process !== "undefined" && process.env.FLY_REGION)
+    ? "https://gql.accsaber.com/graphql"
+    : "http://accsaber-gql.internal:4000/graphql",
   defaultLocale: "en-AU",
   isBeta: true,
 };
