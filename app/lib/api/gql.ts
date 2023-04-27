b892cb5201e3997dd7623c162c2329ca1c0d607b
George Fischer <me@george.id.au>
2023-04-27 12:12:50 +0200

    Persisted Queries

--- app/lib/api/gql.ts
+++ app/lib/api/gql.ts
@@ -2,8 +2,11 @@ import request, { GraphQLClient } from "graphql-request";
 import config from "./config";
 import type { TypedDocumentNode } from "@graphql-typed-document-node/core";
 import { useQuery, type UseQueryResult } from "@tanstack/react-query";
+import { persistedFetch } from "./persistedFetch";
 
-export const gqlClient = new GraphQLClient(config.gqlURL);
+export const gqlClient = new GraphQLClient(config.gqlURL, {
+  fetch: persistedFetch,
+});
 
 export function useGql<TResult, TVariables>(
   document: TypedDocumentNode<TResult, TVariables>,
@@ -12,6 +15,6 @@ export function useGql<TResult, TVariables>(
   return useQuery(
     [(document.definitions[0] as any).name.value, variables],
     async ({ queryKey }) =>
-      request(config.gqlURL, document, queryKey[1] ? queryKey[1] : undefined)
+      gqlClient.request(document, queryKey[1] ? queryKey[1] : undefined)
   );
 }
---
+++ app/lib/api/persistedFetch.ts
@@ -0,0 +1,51 @@
+import { GraphQLList, parse } from "graphql";
+import {
+  GraphQLRequestContext,
+  GraphQLResponse,
+} from "graphql-request/dist/types";
+
+const { subtle } = globalThis.crypto ?? require("crypto").webcrypto;
+export const persistedFetch: typeof fetch = async (input, init) => {
+  if (!init?.body) return new Response("Missing request body", { status: 400 });
+  const queryInfo = JSON.parse(init.body.toString()) as GraphQLRequestContext;
+
+  const rawBody: ArrayBuffer = new TextEncoder().encode(
+    queryInfo.query.toString()
+  );
+  if (!rawBody) return new Response("Missing request body", { status: 400 });
+
+  const hashedBuffer = await subtle.digest("SHA-256", rawBody);
+  const hashString = [...new Uint8Array(hashedBuffer)]
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+
+  const bodyWithPersistedQuery = {
+    ...queryInfo,
+    extensions: {
+      persistedQuery: {
+        version: 1,
+        sha256Hash: hashString,
+      },
+    },
+  };
+
+  const persistedRequest = new Request(input, {
+    ...init,
+    body: JSON.stringify({ ...bodyWithPersistedQuery, query: undefined }),
+  });
+
+  const res = await fetch(persistedRequest);
+  if (res.status !== 200) return res;
+  const parsedBody = (await res.clone().json()) as GraphQLResponse; // TODO: Response schema validation
+  if (parsedBody.errors?.length) {
+    for (const error of parsedBody.errors) {
+      if (error.extensions.code === "PERSISTED_QUERY_NOT_FOUND")
+        return await fetch(input, {
+          ...init,
+          body: JSON.stringify(bodyWithPersistedQuery),
+        });
+    }
+  }
+
+  return res;
+};
