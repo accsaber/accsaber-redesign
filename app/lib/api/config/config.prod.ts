a2bdf0165a5d9bc38045e0a09aeac3bd3010156d
George Fischer <me@george.id.au>
2023-05-12 20:17:50 +0200

    use fly backend

--- app/lib/api/config/config.prod.ts
+++ app/lib/api/config/config.prod.ts
@@ -1,7 +1,9 @@
 import type ApiConfig from "../../interfaces/config";
 
 const productionConfig: ApiConfig = {
-  apiURL: "https://api.accsaber-dev.ixsen.de",
+  apiURL: !(typeof process !== "undefined" && process.env.FLY_REGION)
+    ? "https://accsaber-backend.fly.dev/"
+    : "http://accsaber-backend.internal:8080/",
   cdnURL: "https://cdn.accsaber-dev.ixsen.de",
   gqlURL: !(typeof process !== "undefined" && process.env.FLY_REGION)
     ? "https://gql.accsaber.com/"
