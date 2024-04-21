f7af308d8920371cfb23306fd60e2f6fea1bda98
George Fischer <me@george.id.au>
2024-04-21 14:43:28 +1000

    weighted ap graph

--- app/components/ApGraph.client.tsx
+++ app/components/ApGraph.client.tsx
@@ -9,7 +9,7 @@ import {
   PointElement,
   Legend,
 } from "chart.js";
-import { useEffect, useMemo, useRef } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import type { ApDataPointFragment } from "$gql";
 
 export interface CategoryScoreData extends Category {
@@ -29,6 +29,9 @@ const ApGraph = ({
   Chart.register(LineElement, LinearScale, PointElement, Legend); // I hate every single thing about this but we get errors otherwise
   const ref = useRef();
 
+  const [useWeightedAp, setUseWeighted] = useState(false)
+  const [showZeroAP, setShowZeroAP] = useState(true)
+
   const datasets = useMemo(
     () =>
       categories.map((i, n) => {
@@ -36,54 +39,69 @@ const ApGraph = ({
           label: i.categoryDisplayName,
           data: data
             .filter((c) => c.categoryName === i.categoryName)
-            .map(({ ap }, n) => [n + 1, ap]),
+            .map(({ ap, weightedAp }, n) => [n + 1, useWeightedAp ? weightedAp : ap])
+            .filter(([_, y]) => showZeroAP || (y ?? 0) > 0.01),
           borderColor: ["#34d399", "#2563eb", "#d946ef"][n],
         };
       }),
-    [data, categories]
+    [data, categories, useWeightedAp, showZeroAP]
   );
 
   return (
-    <Line
-      ref={ref}
-      data={{
-        labels: categories.map((i) => i.categoryDisplayName),
-        datasets,
-      }}
-      options={{
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            display: true,
+    <>
+      <div className="flex flex-row gap-2 items-center">
+        <label className="flex gap-2 items-center">
+          <input type="checkbox" checked={useWeightedAp} onChange={() => setUseWeighted(!useWeightedAp)} />
+          Use Weighted AP
+        </label>
+        <label className="flex gap-2 items-center" style={{
+          display: useWeightedAp ? undefined : "none"
+        }}>
+          <input type="checkbox" checked={showZeroAP} onChange={() => setShowZeroAP(!showZeroAP)} />
+          Show 0AP scores
+        </label>
+      </div>
+      <Line
+        ref={ref}
+        data={{
+          labels: categories.map((i) => i.categoryDisplayName),
+          datasets,
+        }}
+        options={{
+          responsive: true,
+          maintainAspectRatio: false,
+          plugins: {
+            legend: {
+              display: true,
+            },
+            tooltip: {
+              intersect: false,
+              mode: "index",
+            },
           },
-          tooltip: {
+          hover: {
             intersect: false,
             mode: "index",
           },
-        },
-        hover: {
-          intersect: false,
-          mode: "index",
-        },
-        scales: {
-          x: {
-            type: "linear",
-            title: {
-              display: true,
-              text: "Nth Score",
+          scales: {
+            x: {
+              type: "linear",
+              title: {
+                display: true,
+                text: "Nth Score",
+              },
+            },
+            y: {
+              type: "linear",
+              title: {
+                display: true,
+                text: useWeightedAp ? "Weighted AP" : "Raw AP",
+              },
             },
           },
-          y: {
-            type: "linear",
-            title: {
-              display: true,
-              text: "Raw AP",
-            },
-          },
-        },
-      }}
-    />
+        }}
+      />
+    </>
   );
 };
 
