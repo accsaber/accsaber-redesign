5755e2523e118eebff75a5e73bab48c8b2e82533
George Fischer <me@george.id.au>
2024-05-09 10:14:54 +1000

    remix version bump

--- remix.config.js
+++ remix.config.js
@@ -4,9 +4,12 @@ module.exports = {
   serverDependenciesToBundle: ["chartjs-adapter-luxon"],
   postcss: true,
 
+
+
   future: {
     v2_errorBoundary: true,
     v2_headers: true,
-    v2_normalizeFormMethod: true
+    v2_normalizeFormMethod: true,
+    v2_dev: true
   },
 };
