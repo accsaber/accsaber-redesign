76919ba3716bf0883a953030ec8550b97d554f0d
George Fischer <me@george.id.au>
2026-03-20 17:41:42 +1100

    lmao whoops

--- app/lib/api/config/config.prod.ts
+++ app/lib/api/config/config.prod.ts
@@ -2,7 +2,7 @@ import type ApiConfig from "../../interfaces/config";
 
 const productionConfig: ApiConfig = {
   apiURL: "https://api.h2.accsaber.com",
-  campaignsURL: typeof "https://campaigns.h2.accsaber.com",
+  campaignsURL: "https://campaigns.h2.accsaber.com",
   cdnURL: "https://cdn.h2.accsaber.com",
   publicURL: "https://cdn.accsaber.com/_frontend",
   gqlURL: "https://gql.h2.accsaber.com/graphql",
