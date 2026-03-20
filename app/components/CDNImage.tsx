cfe1818272b9dfff9286bfacb967f4e8f981cc13
George Fischer <me@george.id.au>
2026-03-20 17:32:07 +1100

    disable image resizing

--- app/components/CDNImage.tsx
+++ app/components/CDNImage.tsx
@@ -16,24 +16,23 @@ export const getImaginaryURL = (image: CDNSource, format?: string) => {
   const encodedPath = btoa(
     isCDN
       ? new URL(targetURL.pathname, "http://accsaber-media/").toString()
-      : targetURL.toString()
+      : targetURL.toString(),
   );
 
   const targetPath = new URL(
     `o7d/s:${image.width}:${image.height}/${encodedPath}${
       format ? `.${format}` : ""
     }`,
-    config.cdnURL
+    config.cdnURL,
   );
 
   return targetPath;
 };
 
-interface CDNImageProps
-  extends DetailedHTMLProps<
-    ImgHTMLAttributes<HTMLImageElement>,
-    HTMLImageElement
-  > {
+interface CDNImageProps extends DetailedHTMLProps<
+  ImgHTMLAttributes<HTMLImageElement>,
+  HTMLImageElement
+> {
   src: string;
   width: number;
   height: number;
@@ -42,23 +41,7 @@ interface CDNImageProps
 const CDNImage = (props: CDNImageProps) => {
   return (
     // eslint-disable-next-line jsx-a11y/alt-text
-    <img
-      {...props}
-      src={new URL(props.src, config.cdnURL).toString()}
-      srcSet={[1, 1.5, 2]
-        .map(
-          (scale) =>
-            `${getImaginaryURL(
-              {
-                src: props.src,
-                width: props.width * scale,
-                height: props.height * scale,
-              },
-              "webp"
-            )} ${scale}x`
-        )
-        .join(", ")}
-    />
+    <img {...props} src={new URL(props.src, config.cdnURL).toString()} />
   );
 };
 
