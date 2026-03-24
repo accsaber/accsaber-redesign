55007c92cf6a3d28d50a9ff2b43707ef3672d9f6
George Fischer <me@george.id.au>
2026-03-24 20:23:07 +1100

    media url update

--- app/lib/api/config/config.prod.ts
+++ app/lib/api/config/config.prod.ts
@@ -1,9 +1,9 @@
 import type ApiConfig from "../../interfaces/config";
 
 const productionConfig: ApiConfig = {
-  apiURL: "https://api.h2.accsaber.com",
+  apiURL: "https://accsaber.com/api/",
   campaignsURL: "https://campaigns.h2.accsaber.com",
-  cdnURL: "https://cdn.h2.accsaber.com",
+  cdnURL: "https://accsaber.com/media/",
   publicURL: "https://cdn.accsaber.com/_frontend",
   gqlURL: "https://gql.h2.accsaber.com/graphql",
   defaultLocale: "en-AU",
