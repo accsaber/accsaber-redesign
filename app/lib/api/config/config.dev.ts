82aaeecef5c1c8aeda35393e8614287d6e7b6819
George Fischer <me@george.id.au>
2026-06-30 16:48:52 +1000

    update campaign data url

--- app/lib/api/config/config.dev.ts
+++ app/lib/api/config/config.dev.ts
@@ -2,7 +2,7 @@ import type ApiConfig from "../../interfaces/config";
 
 const developmentConfig: ApiConfig = {
   apiURL: "https://api.h2.accsaber.com",
-  campaignsURL: "https://campaigns.h2.accsaber.com",
+  campaignsURL: "https://campaigns.h2.accsaber.com/api/",
   cdnURL: "https://accsaber.com/media/",
   publicURL: "https://cdn.accsaber.com/_frontend",
   gqlURL: "https://gql.h2.accsaber.com/graphql",
